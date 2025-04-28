2f0db833fcd8a50d533daddc2100efbfedf06a7a
Marco Milanese <marcomilanese2003@gmail.com>
2025-04-28 21:40:48 +0200

    Added paths for Colab in Dataset calls
    Added the calls to the SpaceDebrisDataset function with Colab Paths for easy switching

--- Training.py
+++ Training.py
@@ -9,29 +9,31 @@ from torchvision.transforms import ToPILImage
 # Select the gpu if available
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+print(f"Using device: {device}")
+
 # Load the Training and Validation datasets
 TrainingSet = SpaceDebrisDataset('./data/trainTest.csv', './data/lowResTrain1ch', './data/Train1ch')
+# Colab Path
+TrainingSet = SpaceDebrisDataset('/content/thesis-space-debris-project/data/train.csv', '/content/thesis-space-debris-project/data/LowResTrain1ch', '/content/thesis-space-debris-project/data/Train1ch')
 
-
-# testing with lowRes for the hiRes so that the images are the same size
-#TrainingSet = SpaceDebrisDataset('./data/trainTest.csv', './data/lowResTrain', './data/lowResTrain')
 print(len(TrainingSet))
 
 ValSet = SpaceDebrisDataset('./data/valTest.csv', './data/lowResVal1ch', './data/Val1ch')
+# Colab Path
+ValSet = SpaceDebrisDataset('/content/thesis-space-debris-project/data/val.csv', '/content/thesis-space-debris-project/data/LowResVal1ch', '/content/thesis-space-debris-project/data/Val1ch')
+
 print(len(ValSet))
 
-#batch_size = 1024 # batch size chosen as 2^10
-#epochs = 20 # number of epochs specified in the paper
+#batch_size = 264 # batch size chosen as 2^8 good for Colab GPU memory
+#epochs = 20 
 
 #test
 batch_size = 5
 epochs = 20
 
-
 # Load the datasets into dataloaders
 trainDataLoader = DataLoader(TrainingSet, batch_size, shuffle=True)
 valDataLoader = DataLoader(ValSet, batch_size, shuffle=True)
-
 
 model = Autoencoder().to(device)
     
